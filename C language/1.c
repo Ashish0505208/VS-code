65d311e317d181938f3de7ca7b2865536cf9e02f
Ashish0505208 <145243260+Ashish0505208@users.noreply.github.com>
2024-12-09 14:11:53 +0530

    vs update

--- C language/1.c
+++ C language/1.c
@@ -1,10 +1,62 @@
 #include<stdio.h>
+#include<stdbool.h>
+#include<ctype.h>
+#include<string.h>
 int main(){
-   int a,b;
-   scanf("%d%d",&a,&b);
-   printf("Sum is %d",a+b);
-   printf("subtraction is %d",a-b);
-   printf("multiplication is %d",a*b);
-   printf("Division is %d",a/b);
-   return 0;
+    int i,j,n;
+    char a[50];
+    printf("Enter a string: ");
+    scanf("%s",a);
+    int l=strlen(a);
+    bool isvar=true;
+    bool isarray=true;
+    bool isfun=true;
+    
+    if(isalpha(a[0]) || a[0]=='_'){
+        isvar=true;
+    }
+    else{
+        isvar=false;
+    }
+
+    for(i=1;i<l;i++){
+        if(isalpha(a[i]) || isdigit(a[i]) || a[i]=='_'){
+            isvar=true;
+        }
+    }
+
+    if(a[l-3]=='[' && a[l-1]==']' && isdigit(a[l-2])){
+        isarray=true;
+        isvar=false;
+    }
+    else{
+        isarray=false;
+    }
+
+    if(a[l-1]==')' && a[l-2]=='('){
+        isfun=true;
+        isvar=false;
+    }
+    else{
+        isfun=false;
+    }
+
+    if(isvar==true){
+        printf("String is a variable\n");
+    }
+    else{
+        printf("STring is not a variable\n");
+    }
+    if(isarray==true){
+        printf("String is a array\n");
+    }
+    else{
+        printf("String is not a array\n");
+    }
+    if(isfun==true){
+        printf("String is a function\n");
+    }
+    else{
+        printf("String is not a function\n");
+    }
 }
